cfebe8df81197dc3ad61d4c32496ca998d83bd42
Andi Repke <andi.repke@gmail.com>
2017-02-02 13:19:44 -0500

    Clock Rotation

---
+++ Assets/Clock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour 
+{
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion euler = Quaternion.Euler (
+			                   new Vector3 (0f, 0f, Time.time * 360f));
+		transform.localRotation = euler;
+	}
+}
